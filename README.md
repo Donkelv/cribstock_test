f1c0ec1247d4dd75421b5b7b031fa8b907b8803c
Kevin <kevinohiro@gmail.com>
2022-07-13 00:19:36 +0100

    feat: card and report screen thingy

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# cribstock_test
+# Cribstock Test
 
-A new Flutter project.
+Cribstock Test
 
-## Getting Started
+## Note
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+I couldn't get assess to the assets so this is the best i could come up with
--- lib/presentation/screen/card_screen.dart
+++ lib/presentation/screen/card_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cribstock_test/data/constants/color_const.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 class CardScreen extends StatelessWidget {
@@ -6,6 +8,18 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: whiteColor,
+      child: Center(
+        child: Text(
+          "Card screen",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20.0.sp),
+        ),
+      ),
+    );
   }
 }
